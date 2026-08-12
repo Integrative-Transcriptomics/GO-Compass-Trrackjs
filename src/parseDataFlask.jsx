4c52e3c27b5861946a84e82fdb8e9a5cc209fac5
ma-ju-he <mathias.hega@student.uni-tuebingen.de>
2026-08-12 19:34:35 +0200

    add extra error handling for performCorrelation function in case a developer has to deal with mysterious correlation heatmap issues just like me today. (you're welcome, dev from the future)

--- src/parseDataFlask.jsx
+++ src/parseDataFlask.jsx
@@ -8,6 +8,16 @@ import axios from 'axios';
 function performCorrelation(data, callback) {
     axios.post("/correlation", {data: data}).then((response) => {
         callback(response.data);
+    }).catch(error => {
+        if (error.response) {
+            console.log(error.response.data);
+            console.log(error.response.status);
+            console.log(error.response.headers);
+        } else if (error.request) {
+            console.log(error.request);
+        } else {
+            console.log('Error', error.message);
+        }
     })
 }
 
